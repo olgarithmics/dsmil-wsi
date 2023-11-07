3fe6e51509e94591fbf5824a18d89dee226130ef
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2023-11-07 14:54:09 +0000

    alma

---
+++ crop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=crop
+#SBATCH -o comp.out
+#SBATCH -e comp.err
+#SBATCH --partition=compute
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=48:00:00
+#SBATCH --cpus-per-task=24
+
+module use /opt/software/easybuild/modules/all/
+module load Mamba
+source ~/.bashrc
+conda activate dl_torch
+cd /home/ofourkioti/Projects/dsmil-wsi/
+
+#python run.py --dataset camelyon --experiment_name phi --feature_path /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/multi_magnification_project/camelyon_data/tf_feats_256/resnet_feats/h5_files --label_file label_files/camelyon_data.csv --csv_files camelyon_csv_files/splits_0.csv  --epoch 100 --save_dir cam_Saved_model --lambda1 0.00001
+#python run_simclr.py --simclr_path  lipo_SIMCLR_checkpoints --feature_path /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/multi_magnification_project/SAR_data/simclr_imgs/h5_files/  --csv_file lipo_csv_files/splits_0.csv --simclr_batch_size 1024
+python deepzoom_tiler.py -m 0  -b 20 -v jpg -e 56
--- deepzoom_tiler.py
+++ deepzoom_tiler.py
@@ -200,11 +200,11 @@ def nested_patches(img_slide, out_base, level=(0,), ext='jpeg'):
     print('\n Organizing patches')
     img_name = img_slide.split(os.sep)[-1].split('.')[0]
     img_class = img_slide.split(os.sep)[2]
-    n_levels = len(glob.glob('WSI_temp_files/*'))
+    n_levels = len(glob.glob('/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/colon/WSI_temp_files/*'))
     bag_path = os.path.join(out_base, img_class, img_name)
     os.makedirs(bag_path, exist_ok=True)
     if len(level)==1:
-        patches = glob.glob(os.path.join('WSI_temp_files', '*', '*.'+ext))
+        patches = glob.glob(os.path.join('/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/colon/WSI_temp_files', '*', '*.'+ext))
         for i, patch in enumerate(patches):
             patch_name = patch.split(os.sep)[-1]
             shutil.move(patch, os.path.join(bag_path, patch_name))
@@ -212,9 +212,9 @@ def nested_patches(img_slide, out_base, level=(0,), ext='jpeg'):
         print('Done.')
     else:
         level_factor = 2**int(level[1]-level[0])
-        levels = [int(os.path.basename(i)) for i in glob.glob(os.path.join('WSI_temp_files', '*'))]
+        levels = [int(os.path.basename(i)) for i in glob.glob(os.path.join('/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/colon/WSI_temp_files', '*'))]
         levels.sort()
-        low_patches = glob.glob(os.path.join('WSI_temp_files', str(levels[0]), '*.'+ext))
+        low_patches = glob.glob(os.path.join('/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/colon/WSI_temp_files', str(levels[0]), '*.'+ext))
         for i, low_patch in enumerate(low_patches):
             low_patch_name = low_patch.split(os.sep)[-1]
             shutil.move(low_patch, os.path.join(bag_path, low_patch_name))
@@ -227,7 +227,7 @@ def nested_patches(img_slide, out_base, level=(0,), ext='jpeg'):
             high_y_list = list( range(low_y*level_factor, (low_y+1)*level_factor) )
             for x_pos in high_x_list:
                 for y_pos in high_y_list:
-                    high_patch = glob.glob(os.path.join('WSI_temp_files', str(levels[1]), '{}_{}.'.format(x_pos, y_pos)+ext))
+                    high_patch = glob.glob(os.path.join('/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/colon/WSI_temp_files', str(levels[1]), '{}_{}.'.format(x_pos, y_pos)+ext))
                     if len(high_patch)!=0:
                         high_patch = high_patch[0]
                         shutil.move(high_patch, os.path.join(bag_path, low_patch_folder, high_patch.split(os.sep)[-1]))
@@ -242,7 +242,7 @@ def nested_patches(img_slide, out_base, level=(0,), ext='jpeg'):
 if __name__ == '__main__':
     Image.MAX_IMAGE_PIXELS = None
     parser = argparse.ArgumentParser(description='Patch extraction for WSI')
-    parser.add_argument('-d', '--dataset', type=str, default='TCGA-lung', help='Dataset name')
+    parser.add_argument('-d', '--dataset', type=str, default='colon', help='Dataset name')
     parser.add_argument('-e', '--overlap', type=int, default=0, help='Overlap of adjacent tiles [0]')
     parser.add_argument('-f', '--format', type=str, default='jpeg', help='Image format for tiles [jpeg]')
     parser.add_argument('-v', '--slide_format', type=str, default='svs', help='Image format for tiles [svs]')
@@ -256,17 +256,17 @@ if __name__ == '__main__':
     args = parser.parse_args()
     levels = tuple(sorted(args.magnifications))
     assert len(levels)<=2, 'Only 1 or 2 magnifications are supported!'
-    path_base = os.path.join('WSI', args.dataset)
+    path_base = os.path.join('/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/slides/', args.dataset)
     if len(levels) == 2:
-        out_base = os.path.join('WSI', args.dataset, 'pyramid')
+        out_base = os.path.join(path_base, 'pyramid')
     else:
-        out_base = os.path.join('WSI', args.dataset, 'single')
+        out_base = os.path.join(path_base, 'single')
     all_slides = glob.glob(os.path.join(path_base, '*/*.'+args.slide_format)) +  glob.glob(os.path.join(path_base, '*/*/*.'+args.slide_format))
     
     # pos-i_pos-j -> x, y
     for idx, c_slide in enumerate(all_slides):
         print('Process slide {}/{}'.format(idx+1, len(all_slides)))
-        DeepZoomStaticTiler(c_slide, 'WSI_temp', levels, args.base_mag, args.objective, args.format, args.tile_size, args.overlap, True, args.quality, args.workers, args.background_t).run()
+        DeepZoomStaticTiler(c_slide, '/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/colon/WSI_temp', levels, args.base_mag, args.objective, args.format, args.tile_size, args.overlap, True, args.quality, args.workers, args.background_t).run()
         nested_patches(c_slide, out_base, levels, ext=args.format)
         shutil.rmtree('WSI_temp_files') 
     print('Patch extraction done for {} slides.'.format(len(all_slides)))
