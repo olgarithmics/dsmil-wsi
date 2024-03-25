4fcbaca1f74b606da003274bdb56468abe42c993
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2024-03-25 15:01:28 +0000

    train

--- train.sh
+++ train.sh
@@ -15,4 +15,4 @@ conda activate dl_torch
 cd /home/ofourkioti/Projects/dsmil-wsi/simclr
 
 #python run.py --dataset camelyon --experiment_name phi --feature_path /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/multi_magnification_project/camelyon_data/tf_feats_256/resnet_feats/h5_files --label_file label_files/camelyon_data.csv --csv_files camelyon_csv_files/splits_0.csv  --epoch 100 --save_dir cam_Saved_model --lambda1 0.00001
-python run.py --dataset simclr_patches --level high
+python run.py --dataset simclr_patches --level high --multiscale 0
